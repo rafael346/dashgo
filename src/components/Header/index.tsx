d71707dacf77f1adcd124013dfd4ef0a11e0162f
rafael346 <sousa_raf@hotmail.com>
2021-07-07 18:34:35 -0300

    Add: contextSidebar; Responsividade: Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SiderBarDrawerContext';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +25,18 @@ export function Header() {
       px='6'
       align='center'
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label='Open Navigation'
+          icon={<Icon
+            as={RiMenuLine} />}
+          fontSize='24'
+          variant='unstyled'
+          onClick={onOpen}
+          mr='2'
+
+        />
+      )}
       <Logo />
       {isWideVersion ? <SearchBox /> : ''}
       <Flex align='center' ml='auto'>
