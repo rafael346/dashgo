fca5857d51247ad14374cbfb0da1a05e6db6cd3b
rafael346 <sousa_raf@hotmail.com>
2021-07-07 17:45:34 -0300

    Responsividade: Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,20 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+type ProfileProps = {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Rafael Sousa</Text>
-        <Text color='gray.300' fontSize='small'>sousa_raf@hotmail.com</Text>
-      </Box>
+      {showProfileData
+        ?
+        <Box mr='4' textAlign='right'>
+          <Text>Rafael Sousa</Text>
+          <Text color='gray.300' fontSize='small'>sousa_raf@hotmail.com</Text>
+        </Box>
+        : ''
+      }
       <Avatar size='md' name='Rafael' src='https://github.com/rafael346.png' />
     </Flex>
   )
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,8 +1,8 @@
 import { Box, Stack } from "@chakra-ui/react";
 import React from "react";
 import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
-import { NavLink } from "../SideBar/NavLink";
-import { NavSection } from "../SideBar/NavSection";
+import { NavLink } from "./NavLink";
+import { NavSection } from "./NavSection";
 
 export function Sidebar() {
   return (
